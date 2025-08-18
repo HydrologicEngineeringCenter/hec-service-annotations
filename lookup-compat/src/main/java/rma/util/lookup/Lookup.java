4063cf4122d310224e04d4ecef084b6b7814b8b5
Stephen Ackerman <stephen@rmanet.com>
2025-08-18 15:37:22 -0700

    RSS-1244: Fix high CPU bug in Java 21+ with HEC Lookup by providing proxy to upstream Netbeans library
    The old Lookup library is a fork of the upstream Netbeans library. Since the Netbeans library is now distributed standalone, there's no reason to carry the fork of it anymore.
    
    Provide a compatibility layer which can be swapped in via dependency substitution for libraries which have not been updated so that they will call the new implementation and avoid excessive CPU use on current versions of Java. This is not a 100% complete implementation, and should not be used for compilation. This is purely a runtime substitution so that older code will call the new library until such a time that it can be updated.
    
    Also, remove the old, unnecessary fork of the library. Consumers of the lookup library are advised to switch to the upstream Netbeans library directly.

---
+++ lookup-compat/src/main/java/rma/util/lookup/Lookup.java
@@ -0,0 +1,40 @@
+package rma.util.lookup;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * This is a transitional class to provide temporary compatibility with HEC codebases until they can upgrade to the
+ * upstream Netbeans Lookup library.
+ * <br><br>
+ * Originally, NetBeans did not ship the Lookup library as a standalone library. Since it provided useful functionality,
+ * the Lookup code was duplicated into the package rma.util for use in some application development. Over time,
+ * adoption of this library spread across many HEC libraries and applications.
+ * <br><br>
+ * Today, Netbeans ships Lookup as a standalone library, so this duplicated copy of the code is no longer necessary.
+ * Unfortunately, updating the code across every relevant application and library takes time. The legacy copy
+ * of the code in rma.util has bugs with recent versions of Java  which have since been solved in the upstream codebase.
+ * Rather than continuing to maintain an unnecessary fork indefinitely, this class provides a minimal Proxy which allows
+ * code compiled against the old library to call the upstream library until a time when it can be updated.
+ * <br><br>
+ * Applications that move to Java 21+ but still depend on libraries that use the old rma.util Lookup code
+ * should use a dependency substitution to swap this library in to replace the old Lookup library. Codebases should
+ * avoid compiling against this library, and it should be used purely for transitional purposes.
+ *
+ * @deprecated Use @{@link org.openide.util.Lookup} instead.
+ * @author Stephen Ackerman, GEI Consultants
+ */
+@Deprecated(forRemoval = true, since = "3.0.0")
+public abstract class Lookup {
+
+    @Deprecated(forRemoval = true, since = "3.0.0")
+    public static Lookup getDefault() {
+        return new DelegatingLookup(org.openide.util.Lookup.getDefault());
+    }
+
+    @Deprecated(forRemoval = true, since = "3.0.0")
+    public abstract <T> T lookup(Class<T> clazz);
+
+    @Deprecated(forRemoval = true, since = "3.0.0")
+    public abstract <T> Collection<? extends T> lookupAll(Class<T> clazz);
+}
